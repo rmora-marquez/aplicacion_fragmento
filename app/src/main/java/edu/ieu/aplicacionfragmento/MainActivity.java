92b8320631ccb97f20f8d819a0e8931561b97543
Ramon Mora Marquez <60113219+rmora-marquez@users.noreply.github.com>
2021-10-27 18:52:55 -0500

    fragmento dinamico completado

--- app/src/main/java/edu/ieu/aplicacionfragmento/MainActivity.java
+++ app/src/main/java/edu/ieu/aplicacionfragmento/MainActivity.java
@@ -1,14 +1,28 @@
 package edu.ieu.aplicacionfragmento;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import edu.ieu.aplicacionfragmento.fragment.CountFragment;
 
 public class MainActivity extends AppCompatActivity {
+    private Button botonAgregar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        botonAgregar = findViewById(R.id.boton_agregar);
+        botonAgregar.setOnClickListener( view -> {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            CountFragment countFragment = new CountFragment();
+            transaction.add(R.id.main_activity, countFragment);
+            transaction.commit();
+        });
     }
 }
